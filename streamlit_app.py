9cf12be443247cb38ca0c6b12f0fc5a4eb16b3c0
areejsa <34766091+areejsa@users.noreply.github.com>
2026-03-15 02:35:23 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col,when_matched
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
+
+
+
+session = get_active_session()
+
+my_dataframe = session.table("smoothies.public.orders")\
+    .filter(col("ORDER_FILLED") == 0) \
+   .collect()
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            og_dataset.merge(edited_dataset
+                 , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                 , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                )
+            st.success("Order(s) updated!", icon="✅")
+        except:
+            st.write('O oh something wrong')
+
+
+        
+
+else:
+    st.success("No orders !!")
